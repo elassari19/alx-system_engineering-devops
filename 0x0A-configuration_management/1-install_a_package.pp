748f6faf770ec2bd78f27ab31a4dc060b79a657d
elassari19 <elassari19@gmail.com>
2023-11-24 19:19:36 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 # Using Puppet, install flask from pip3
 # Requirements: Install flask, Version must be 2.1.0
 
-exec { 'puppet-lint':
+package {'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
