2a0cfb8853dc9d9919d86703e1909c2c7424cde4
elassari19 <elassari19@gmail.com>
2023-11-24 19:17:23 +0100

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -2,5 +2,6 @@
 # Requirements: Install flask, Version must be 2.1.0
 
 exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+  ensure   => '2.1.0',
+  provider => 'gem'
 }
