2169153181356fc2625ec3b48a1bc28125cdc2f5
elassari19 <elassari19@gmail.com>
2023-11-24 19:13:50 +0100

    0x0A-configuration_management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,7 @@
+# Using Puppet, create a manifest that kills a process named killmenow
+# Requirements: Must use the exec Puppet resource ,Must use pkill
+
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
+}
