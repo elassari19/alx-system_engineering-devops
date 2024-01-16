710ef3427afd9e0d460667c3c4543b78e94c106a
elassari19 <elassari19@gmail.com>
2024-01-16 23:35:36 +0100

    0x16-api_advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -15,14 +15,15 @@ def top_ten(subreddit):
     '''
     should return the title of ten posts
     '''
+    url = "https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit)
     header = {'User-Agent': 'CustomClient/1.0'}
-    res = requests.get(BASE_URL+"/r/{}?sort=hot&limit=10".format(subreddit),
-                       headers=header, allow_redirects=False)
-    if res.status_code != 200:
-        return (0)
-    res = res.json()
-    if "data" in res:
-        for post in res.get("data").get("children"):
+    req = requests.get(url, headers=header, allow_redirects=False)
+    if req.status_code != 200:
+        print(None)
+        return
+    req = req.json()
+    if "data" in req:
+        for post in req.get("data").get("children"):
             print(post.get("data").get("title"))
     else:
         print(None)
