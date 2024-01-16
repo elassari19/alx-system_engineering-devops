cffe467074a81197f64b7173b4d595809f76acb5
elassari19 <elassari19@gmail.com>
2024-01-17 00:15:07 +0100

    0x17-web_stack_debugging_3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# file should contain Puppet code
+# You can use whatever Puppet resource type you want for you fix
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/'],
+}
