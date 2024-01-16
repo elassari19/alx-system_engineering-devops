ce3f075f93541310deb189a9d3fe39385b92bcc6
elassari19 <elassari19@gmail.com>
2024-01-17 00:00:12 +0100

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,12 +14,10 @@ def number_of_subscribers(subreddit):
     if subreddit is None or not isinstance(subreddit, str):
         return 0
 
-    url = 'https://www.reddit.com/r//about.json'.format(subreddit)
-    response = get(url)
-    results = response.json()
-
-    try:
-        return results.get('data').get('subscribers')
-
-    except Exception:
+    response = get('https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    if (not response.ok):
         return 0
+    
+    results = response.json()
+    count = results.get('data').get('subscribers')
+    return count
